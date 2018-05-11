43be2d5bff2a3e81680264e12cc80f2a3f006a25
sdominicq <dquintero@student.sierracollege.edu>
2018-05-11 08:35:52 -0700

    attempt

--- app/src/main/java/com/example/rgarcia21/weatherappandroid/MainActivity.java
+++ app/src/main/java/com/example/rgarcia21/weatherappandroid/MainActivity.java
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void getWeatherButton(View v){
         EditText location = (EditText) findViewById(R.id.searchBar);
-        if (location.getText() == null){
+        if (location.getText().toString().equals("")){
             Toast toast2 = Toast.makeText(getApplicationContext(), "Please enter Input", Toast.LENGTH_SHORT);
             toast2.show();
         } else{
